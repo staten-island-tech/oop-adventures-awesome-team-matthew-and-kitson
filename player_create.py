ea82815849250a17fbf6d6b6c139f4e556512e79
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2023-11-17 14:01:12 -0500

    Update player_create.py

--- player_create.py
+++ player_create.py
@@ -2,16 +2,26 @@ import json
 import os
 import random
 PName=input("What is the player's name? ")
-
-class Player:
-    Name=(PName)
-    Mainq=input("Which player type do you want to be; Warrior, Magical, or Archer?")
-class Warrior(Player):
+Attributes={}
+Mainq=input("Which player type do you want to be; Warrior, Magical, or Archer?")
+class Warrior():
     if Mainq=="Warrior":
         Wq=input("Do you want to be a Knight or Tank?")
+        if Wq=="Knight":
+            with open("data.json", "r") as f:
+                data = json.load(f)
+                print(data[1])
+                KAppend=data[1]
+                Attributes.append("Player Name": PName, KAppend)
+                data.append(Attributes)
+        elif Wq=="Tank":
+            print("e")
+        else:
+            print("Error")
 #import
 with open("data.json", "r") as f:
     data = json.load(f)
+    data.append(Attributes)
 
 with open("data.json", "r") as f:
     new_file = "updated.json"
