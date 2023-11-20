3a30bae16cdb13bbfcba07d6de2739fc9ba2e31f
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2023-11-20 13:26:45 -0500

    Update player_create.py

--- player_create.py
+++ player_create.py
@@ -8,7 +8,7 @@ class Warrior():
     if Mainq=="Warrior":
         Wq=input("Do you want to be a Knight or Tank?")
         if Wq=="Knight":
-            with open("data.json", "r") as f:
+            with open("class.json", "r") as f:
                 data = json.load(f)
                 print(data[1])
                 KAppend=data[1]
@@ -19,14 +19,14 @@ class Warrior():
         else:
             print("Error")
 #import
-with open("data.json", "r") as f:
+with open("class.json", "r") as f:
     data = json.load(f)
     data.append(Attributes)
 
-with open("data.json", "r") as f:
+with open("class.json", "r") as f:
     new_file = "updated.json"
     with open(new_file, "w") as f:
         json_string = json.dumps(data, indent=4)
         f.write(json_string)
-    os.remove("data.json")
-    os.rename(new_file, "data.json")
+    os.remove("class.json")
+    os.rename(new_file, "class.json")
