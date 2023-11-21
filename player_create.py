9822eb1c53ec218906503407f0e9024a4e428cc9
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2023-11-20 21:59:13 -0500

    Update player_create.py

--- player_create.py
+++ player_create.py
@@ -4,29 +4,49 @@ import random
 PName=input("What is the player's name? ")
 Attributes={"Player Name": PName}
 print(Attributes)
+
+
+with open("class.json", "r") as f:
+    data = json.load(f)
+    class Player:
+        def Create(self, i):
+            PName=data[i]
+            HP=
+            Weapon1=
+            Moves1=
+            Weapon2=
+            Moves2=
+            if "Weapon 2" in data[i]:
+                PValues={'PName:':PName,'HP:':HP,'Weapon1:':Weapon1,'Moves1:':Moves1,'Rating:':rating}
+            else:
+                PValues={'PName:':PName,'HP:':HP,'Weapon1:':Weapon1,'Moves1:':Moves1}
+            return(PValues)
+    def __str__(thing):
+        return f"{thing.name}, {thing.company}"
+with open("player.json", "r") as f:
+    data = json.load(f)
+    P= Player()
+    PValues=P.Create()
+    data.append(PValues)
+
+with open("player.json", "r") as f:
+    new_file = "updated.json"
+    with open(new_file, "w") as f:
+        json_string = json.dumps(data, indent=4)
+        f.write(json_string)
+    os.remove("player.json")
+    os.rename(new_file, "player.json")
+
+
 Mainq=input("Which player type do you want to be; Warrior, Magical, or Archer?")
 class Warrior():
     if Mainq=="Warrior":
         Wq=input("Do you want to be a Knight or Tank?")
         if Wq=="Knight":
-            with open("class.json", "r") as f:
-                data = json.load(f)
-                print(data[1])
-                KAppend=data[1]
-                data.append(Attributes)
+            print("e")
         elif Wq=="Tank":
             print("e")
         else:
             print("Error")
 #import
-with open("class.json", "r") as f:
-    data = json.load(f)
-    data.append(Attributes)
 
-with open("class.json", "r") as f:
-    new_file = "updated.json"
-    with open(new_file, "w") as f:
-        json_string = json.dumps(data, indent=4)
-        f.write(json_string)
-    os.remove("class.json")
-    os.rename(new_file, "class.json")
