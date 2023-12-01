5532e4287c53741aa29d8aeed2842aa79a7e9641
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2023-12-01 13:58:21 -0500

    Update moves.py

--- moves.py
+++ moves.py
@@ -1,8 +1,29 @@
 import random
 import json
-with open("player.json", "r") as pjson:
-    data = json.load(pjson)
-    def swordswing(x, y):
-        player=data[8]
-        enemy=y
-        
+class moves():
+    def __init__(self, player, enemy, damage):
+        self.player = player
+        self.enemy = enemy
+        self.damage = damage
+class playermoves(moves):
+    def swordswing(y):
+        with open("player.json", "r") as pjson:
+            data = json.load(pjson)
+            player=data[8]
+            enemy=y
+class playereffectmoves(moves):
+      print("FILLLLEEEERRRR")
+class basicenemymoves(moves):
+    def goblinattack():
+        with open("player.json", "r") as pjson:
+            data = json.load(pjson)
+            player=data[8]
+            with open("data.json", "r") as ejson:
+                edata = json.load(ejson)
+                enemy=edata[0]
+                player['CurrentHP:']=player['CurrentHP:']-enemy['Dmg:']
+                print(player['CurrentHP:'])
+class bossmoves(moves):
+    print("FILLLLLLEEEERRRR")
+e=basicenemymoves
+e.goblinattack()
