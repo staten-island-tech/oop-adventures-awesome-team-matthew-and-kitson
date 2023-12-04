ac0003648516cf9634291ca24fad8bb663a4aac3
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2023-12-04 13:48:24 -0500

    Update moves.py

--- moves.py
+++ moves.py
@@ -1,8 +1,12 @@
 import random
 import json
+
 class moves():
-    def __init__(self, player, enemy, damage):
-        self.player = player
+    def __init__(self, php, enemy, damage):
+        with open("player.json", "r") as pjson:
+            data = json.load(pjson)
+            player=data[8]
+        self.php = player['ModifiedHP']
         self.enemy = enemy
         self.damage = damage
 class playermoves(moves):
@@ -21,9 +25,11 @@ class basicenemymoves(moves):
             with open("data.json", "r") as ejson:
                 edata = json.load(ejson)
                 enemy=edata[0]
-                player['Modified:']=player['ModifiedHP:']-enemy['Dmg:']
+                basicenemymoves.php=player['ModifiedHP:']-enemy['Dmg:']
+                print(player['Modified:'])
 
 class bossmoves(moves):
     print("FILLLLLLEEEERRRR")
 e=basicenemymoves
+e.goblinattack()
 e.goblinattack()
