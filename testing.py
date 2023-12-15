192bfecc7722b435d6c3542b8fc5c8f7027b18cf
taskdire <136046239+taskdire@users.noreply.github.com>
2023-12-15 13:50:32 -0500

    123
    123

--- testing.py
+++ testing.py
@@ -2,15 +2,16 @@ import os
 import time
 import random
 
-
-s=random.randit(0,10)
-
+s=random.randint(1,3)
+e=input("ENTER Block:")
 while s<=60:
-    os.system('cls')
     print (s, 'Seconds')
     time.sleep(1)
-    s+=1
+    print ("\033[A                             \033[A")
+    s-=1
     if s==0:
         print("Failed to Block")
         break
-   
+    if e=="Block":
+        print("Blocked!")
+        break
