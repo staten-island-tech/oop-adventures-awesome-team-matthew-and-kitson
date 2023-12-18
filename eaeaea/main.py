e8629d16c670bc23e37a3706ce2562a6e06f508f
taskdire <136046239+taskdire@users.noreply.github.com>
2023-12-18 13:58:01 -0500

    123

--- eaeaea/main.py
+++ eaeaea/main.py
@@ -1,8 +1,28 @@
 from merchant import Merchant
 from hero import Hero
+import json
+
 guy=Merchant("Filler", ["1", "2", "3", "4"])
 me=Hero("Me", 500, [])
-guy.welcome()
+guy.welcome
 
-guy.sell("3")
-me.buy("3")
+
+
+
+with open("item.json", "r") as f:
+    # Serialize the updated Python list to a JSON string
+    data = json.load(f)
+
+find = input("What Do you want to buy/sell: ").capitalize()
+List = []
+money=len(data)
+
+
+for i in range(money): 
+    if find in data[i]['Name']:
+        List.extend([data[i]])
+if not List:
+    print("No Value")
+else:
+    print() 
+
