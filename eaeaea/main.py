41d097c1269f6d893b47e2bfd6d0ad76288c7505
taskdire <136046239+taskdire@users.noreply.github.com>
2023-12-21 12:54:18 -0500

    awdawda
    awdawdawdad

--- eaeaea/main.py
+++ eaeaea/main.py
@@ -6,25 +6,30 @@ guy=Merchant("Name", ["ruby", "diamond", "silver_band", "gold_band"])
 me=Hero("Me", 500, [])
 guy.welcome
 
-find = input("What Do you want to buy/sell: ")
-if find in guy.product:
-    print(True)
+class merchant(): 
 
-
-with open("item.json", "r") as f:
+    with open("item.json", "r") as f:
     # Serialize the updated Python list to a JSON string
-    data = json.load(f)
+        data = json.load(f)
+    guy=Merchant("Name", ["ruby", "diamond", "silver_band", "gold_band"])
+    me=Hero("Me", 500, [])
+    guy.welcome
+
+    find = input("What Do you want to buy/sell: ").lower()
+    if find in guy.product:
+        print(True)
 
 
 
-
-
+    item = None
  
-for i in (data): 
-    if find in i['Name']:
-        data.append(i[data])
-if not data:
-    print("No Value")
-else:
-    print("You have Sold 'Name' for 'Price'") 
+    for i in (data): 
+        if find in i['Name']:
+            item = i['Name']
+    if item == find:
+        print("you have sold 'Name' for 'price' ")
+    if item == None:
+        print("you do not have this item or it is of no value")
+
+
 
