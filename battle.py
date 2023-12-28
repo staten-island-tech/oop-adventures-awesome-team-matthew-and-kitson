b79269a2a53fe3d63a9fcefc6d3e60647a910de5
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2023-12-28 16:41:03 -0500

    Delete battle.py

--- battle.py
+++
@@ -1,7 +0,0 @@
-from moves import moves
-from moves import playermoves
-with open("player.json", "r") as pjson:
-    input=input("What move would you like to use? ")
-    for i in range(len(pjson[8]['Moves1:'])):
-        if pjson[8]['Moves1:']==input:
-            if 
