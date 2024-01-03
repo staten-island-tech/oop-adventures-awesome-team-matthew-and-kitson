fc035baa00a0f3137335a39009877066a10d8739
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-01-02 21:54:36 -0500

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,4 +1,5 @@
 import json
+from decimal import Decimal
 class items():
     def __init__(self, Name):
         self.Name = Name.capitalize()
@@ -6,8 +7,9 @@ class Moneyitem(items):
     def __init__(self, Name, Price):
         self.Name = Name.capitalize()
         self.Price = Price
-with open("item.json", "r") as ijson:
-    data=json.load(ijson)
+with open("player.json", "r") as pjson:
+    data = json.load(pjson)
+    player=data[8]
     Trinket=Moneyitem("Trinket", 19)
     Gold_Goblet=Moneyitem("Gold Goblet", 43)
     Silver_Goblet=Moneyitem("Silver Goblet", 18)
@@ -47,7 +49,7 @@ with open("item.json", "r") as ijson:
             self.Name = Name
             self.HP = HP
             self.Price=Price
-    Trousers=Leggings(["Mage", "Necromancer"], "Trousers", 8, 43)
+    Trousers=Leggings(["Mage", "Necromancer", "Knight"], "Trousers", 8, 43)
     LeatherLeggings=Leggings(["Healer", "SpellCaster"], "Leather Leggings", 10, 56)
     PlateLeggings=Leggings(["Knight", "Tank"], "Plated Leggings", 23, 86)
     LeggingsList=[PlateLeggings, LeatherLeggings, Trousers]
@@ -62,28 +64,41 @@ with open("item.json", "r") as ijson:
     PlateBoots=Boots(["Knight", "Tank"], "Plated Boots", 19, 29)
     BootsList=[PlateBoots, HeavyBoots, LeatherBoots]
 class Enemy():
-    def __init__(self, Name, JSON_Num, HP, DMG, EXP, Gold):
-        self.name = Name.capitalize()
-        self.JSON_Num = JSON_Num
+    def __init__(self, Name, Num, HP, DMG, EXP, Gold):
+        self.Name = Name.capitalize()
+        self.Num = Num
         self.HP = HP
         self.DMG = DMG
         self.EXP = EXP
         self.Gold=Gold
 class Boss(Enemy):
-    def __init__(self, Name, JSON_num, HP, EXP, Gold):
+    def __init__(self, Name, Num, HP, EXP, Gold):
         self.Name = Name.capitalize()
-        self.JSON_num = JSON_num
+        self.Num = Num
         self.HP = HP
         self.EXP = EXP
         self.Gold=Gold
-with open("data.json", "r") as djson:
-    data = json.load(djson)
-    goblin=Enemy(data[0]["Name:"], 0, data[0]["Hp:"], data[0]["Dmg:"], 5, 1)
-    spider=Enemy(data[1]["Name:"], 1, data[1]["Hp:"], data[1]["Dmg:"], 10, 2)
-    slime=Enemy(data[2]["Name:"], 2, data[2]["Hp:"], data[2]["Dmg:"], 15, 2)
-    zombie=Enemy(data[3]["Name:"], 3, data[3]["Hp:"], data[3]["Dmg:"], 20, 5)
-    Golem=Boss(data[4]["Name:"], 4, data[4]["Hp:"], 50, 50)
-    Lich=Boss(data[5]["Name:"], 5, data[5]["Hp:"], 70, 50)
-    Dragon=Boss(data[6]["Name:"], 6, data[6]["Hp:"], 200, 100)
-    King=Boss(data[8]["Name:"], 8, data[8]["Hp:"], 250, 100)
-    Hydra=Boss(data[10]["Name:"], 10, data[10]["Hp:"], 400, 200)
+goblin=Enemy("Goblin", 0, 32, 12, 5, 1)
+spider=Enemy("Spider", 1, 24, 16, 10, 2)
+slime=Enemy("Slime", 2, 12, 5, 15, 2)
+zombie=Enemy("Zombie", 3, 43, 27, 20, 5)
+Golem=Boss("Golem", 4, 238, 50, 50)
+Lich=Boss("Lich", 5, 150, 70, 50)
+Dragon=Boss("Dragon", 6, 534, 200, 100)
+King=Boss("King", 8, 367, 250, 100)
+Hydra=Boss("Hydra", 10, 462, 400, 200)
+class BattleHero:
+    def __init__(self, name, hp, hpmultiplier, damagemultiplier, moves):
+        self.name = name
+        self.hp = hp
+        self.hpmultiplier=hpmultiplier
+        self.damagemultiplier = damagemultiplier
+        self.moves=moves
+class Merchant:
+    def __init__(self, products):
+        self.products=products
+    def sell(self, item):
+        self.products.remove(item)
+    def welcome(x):
+        print(f"Welcome to my shop! You can buy items from my catalogue, or sell any sellable item here!")
+Hero=BattleHero(player['PName:'], round(Decimal(player['BaseHP:']*player['HpMultiplier:'])), player['HpMultiplier:'], player['DmgMultiplier:'], player['Moves:'])
