d46430d9a8f83778d61a0dd5aadb7bdcf70816bf
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-01-02 21:54:19 -0500

    Update equip.py

--- equip.py
+++ equip.py
@@ -27,6 +27,7 @@ def equip():
                     pjson.write(json_string)
                 os.remove("player.json")
                 os.rename(new_file, "player.json")
+                print("Successfully equipped!")
             else:
                 print("Boots already equipped, not in inventory, or unequipable for your class.")
         if armorcheck=="Leggings":
@@ -36,7 +37,7 @@ def equip():
             for i in range(len(stuff.LeggingsList)):
                     if leggingscheck in stuff.LeggingsList[i].Name:
                         selectleggings=stuff.LeggingsList[i]
-            if leggingscheck in player['Inventory:'] and player['Legging:'] == "N/A" and player['Class:'] in selectleggings.Equipable:
+            if leggingscheck in player['Inventory:'] and player['Leggings:'] == "N/A" and player['Class:'] in selectleggings.Equipable:
                 player['Leggings:']=selectleggings.Name
                 player['BaseHP:']=player['BaseHP:']+selectleggings.HP
                 new_file = "updated.json"
@@ -45,6 +46,7 @@ def equip():
                     pjson.write(json_string)
                 os.remove("player.json")
                 os.rename(new_file, "player.json")
+                print("Successfully equipped!")
             else:
                 print("Leggings already equipped, not in inventory, or unequipable for your class.")
         if armorcheck=="Chestplate":
@@ -54,7 +56,7 @@ def equip():
             for i in range(len(stuff.ChestplateList)):
                     if chestplatecheck in stuff.ChestplateList[i].Name:
                         selectchestplate=stuff.ChestplateList[i]
-            if chestplatecheck in player['Inventory:'] and player['Chesplate:'] == "N/A" and player['Class:'] in selectchestplate.Equipable:
+            if chestplatecheck in player['Inventory:'] and player['Chestplate:'] == "N/A" and player['Class:'] in selectchestplate.Equipable:
                 player['Chestplate:']=selectchestplate.Name
                 player['HpMultiplier:']=player['HpMultiplier:']+selectchestplate.HP
                 new_file = "updated.json"
@@ -63,6 +65,7 @@ def equip():
                     pjson.write(json_string)
                 os.remove("player.json")
                 os.rename(new_file, "player.json")
+                print("Successfully equipped!")
             else:
                 print("Chestplae already equipped, not in inventory, or unequipable for your class.")
         if armorcheck=="Helmet":
@@ -81,6 +84,7 @@ def equip():
                     pjson.write(json_string)
                 os.remove("player.json")
                 os.rename(new_file, "player.json")
+                print("Successfully equipped!")
             else:
                 print("Helmet already equipped, not in inventory, or unequipable for your class.")
     elif check=="Weapon":
@@ -101,13 +105,81 @@ def equip():
                     pjson.write(json_string)
                 os.remove("player.json")
                 os.rename(new_file, "player.json")
+                print("Successfully equipped!")
         else:
             print("Max weapons already equipped.")
 def unequip():
     check=input("Would you like to unequip a Weapon or Armor? (Enter to go break) " )
     if check=="Armor":
         armorcheck=input("Would you like to unequip Boots, Leggings, a Chestplate, or a Helmet? ")
-
+        if armorcheck=="Boots":
+            bootscheck=player['Boots:']
+            if not bootscheck=="N/A":
+                for i in range(len(stuff.BootsList)):
+                    if bootscheck in stuff.BootsList[i].Name:
+                        selectboots=stuff.BootsList[i]
+                player['Boots:']="N/A"
+                player['BaseHP:']=player['BaseHP:']-selectboots.HP
+                new_file = "updated.json"
+                with open(new_file, "w") as pjson:
+                    json_string = json.dumps(data, indent=4)
+                    pjson.write(json_string)
+                os.remove("player.json")
+                os.rename(new_file, "player.json")
+                print("Successfully unequipped!")
+            else:
+                print("Boots are not equipped.")
+        if armorcheck=="Leggings":
+            leggingscheck=player['Leggings:']
+            if not leggingscheck=="N/A":
+                for i in range(len(stuff.LeggingsList)):
+                    if leggingscheck in stuff.LeggingsList[i].Name:
+                        selectleggings=stuff.LeggingsList[i]
+                player['Leggings:']="N/A"
+                player['BaseHP:']=player['BaseHP:']-selectleggings.HP
+                new_file = "updated.json"
+                with open(new_file, "w") as pjson:
+                    json_string = json.dumps(data, indent=4)
+                    pjson.write(json_string)
+                os.remove("player.json")
+                os.rename(new_file, "player.json")
+                print("Successfully unequipped!")
+            else:
+                print("Leggings are not equipped.")
+        if armorcheck=="Chestplate":
+            chestplatechcek=player['Chestplate:']
+            if not chestplatechcek=="N/A":
+                for i in range(len(stuff.ChestplateList)):
+                    if chestplatechcek in stuff.ChestplateList[i].Name:
+                        selectchestplate=stuff.ChestplateList[i]
+                player['Chestplate:']="N/A"
+                player['HpMultiplier:']=player['HpMultiplier:']-selectchestplate.HP
+                new_file = "updated.json"
+                with open(new_file, "w") as pjson:
+                    json_string = json.dumps(data, indent=4)
+                    pjson.write(json_string)
+                os.remove("player.json")
+                os.rename(new_file, "player.json")
+                print("Successfully unequipped!")
+            else:
+                print("Chestplate are not equipped.")
+        if armorcheck=="Helmet":
+            helmetcheck=player['Helmet:']
+            if not helmetcheck=="N/A":
+                for i in range(len(stuff.HelmetList)):
+                    if helmetcheck in stuff.HelmetList[i].Name:
+                        selecthelmet=stuff.HelmetList[i]
+                player['Helmet:']="N/A"
+                player['BaseHP:']=player['BaseHP:']-selecthelmet.HP
+                new_file = "updated.json"
+                with open(new_file, "w") as pjson:
+                    json_string = json.dumps(data, indent=4)
+                    pjson.write(json_string)
+                os.remove("player.json")
+                os.rename(new_file, "player.json")
+                print("Successfully unequipped!")
+            else:
+                print("Helmet is not equipped.")
     elif check=="Weapon":
         if not player['Weapon2:']=="N/A":
             weaponcheck=input("What weapon would you like to unequip? ")
@@ -131,6 +203,7 @@ def unequip():
                 pjson.write(json_string)
             os.remove("player.json")
             os.rename(new_file, "player.json")
+            print("Successfully unequipped!")
         else:
             print("You cannot unequip a weapon with only 1 weapon equipped.")
 
