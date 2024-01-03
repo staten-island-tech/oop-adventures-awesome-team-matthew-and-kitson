7ae3c73351ba5ca7a9024ca1bbfc15440dbf5a3a
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-01-02 21:54:31 -0500

    Update player_create.py

--- player_create.py
+++ player_create.py
@@ -1,10 +1,10 @@
 import json
 import os
-PName=input("What is the player's name? ")
 def confirm(i):
     print("Congratulations, you have selected & became the class:", i)
 with open("player.json", "r") as pjson:
     data = json.load(pjson)
+    PName=input("What is the player's name? ")
     class Player:
         def Create(self, i):
             PlayerName=PName
@@ -31,75 +31,69 @@ with open("player.json", "r") as pjson:
             os.remove("player.json")
             os.rename(new_file, "player.json")
         def info_check(self, x):
+            P= Player()
             with open("player.json", "r") as pjson:
                 PValues=P.Create(x)
                 print(PValues)
-
-P= Player()
-e=True
-while e ==True:
-    Mainq=input("Which player type do you want to be; Warrior, Magical, or Archer? Enter 'Info' for info on classes. ")
-    if Mainq=="Warrior" or Mainq=="Magical" or Mainq=="Archer":
-        break
-    elif Mainq=='Info':
-        with open("player.json", "r") as pjson:
-            data = json.load(pjson)
-            for i in range(len(data)):
-                print(data[i])
-    else:
-        print("Enter a valid class.")
-class Warrior(Player):
-    if Mainq=="Warrior":
-        x=True
-        while x ==True:
-            Wq=input("Do you want to be a Knight or Tank? ")
-            if Wq=="Knight":
-                P.Create2(0)
-                confirm(Wq)
-                break
-            elif Wq=="Tank":
-                P.Create2(1)
-                confirm(Wq)
-                break
-            else:
-                print("Error, please enter one of the options.")
-
-class Magical(Player):
-    if Mainq=="Magical":
-        x=True
-        while x ==True:
-            Mq=input("Do you want to be a Mage, Necromancer, Spellcaster, or Healer? ")
-            if Mq=="Mage":
-                P.Create2(2)
-                confirm(Mq)
-                break
-            elif Mq=="Necromancer":
-                P.Create2(3)
-                confirm(Mq)
-                break
-            elif Mq=="Spellcaster":
-                P.Create2(4)
-                confirm(Mq)
-                break
-            elif Mq=="Healer":
-                P.Create2(5)
-                confirm(Mq)
-                break
-            else:
-                print("Error, please enter one of the options.")
-
-class Archer(Player):
-    if Mainq=="Archer":
-        x=True
-        while x ==True:
-            Aq=input("Do you want to be a Archer or Slinger (Slingshot)? ")
-            if Aq=="Archer":
-                P.Create2(6)
-                confirm(Aq)
-                break
-            elif Aq=="Slinger":
-                P.Create2(7)
-                confirm(Aq)
-                break
-            else:
-                print("Error, please enter one of the options.")
+        def ultcreate():
+            P= Player()
+            e=True
+            while e ==True:
+                Mainq=input("Which player type do you want to be; Warrior, Magical, or Archer? Enter 'Info' for info on classes. ")
+                if Mainq=='Info':
+                    with open("player.json", "r") as pjson:
+                        data = json.load(pjson)
+                        for i in range(len(data)):
+                            print(data[i])
+                elif Mainq=="Warrior":
+                    x=True
+                    while x ==True:
+                        Wq=input("Do you want to be a Knight or Tank? ")
+                        if Wq=="Knight":
+                            P.Create2(0)
+                            confirm(Wq)
+                            break
+                        elif Wq=="Tank":
+                            P.Create2(1)
+                            confirm(Wq)
+                            break
+                        else:
+                            print("Error, please enter one of the options.")
+                elif Mainq=="Magical":
+                    x=True
+                    while x ==True:
+                        Mq=input("Do you want to be a Mage, Necromancer, Spellcaster, or Healer? ")
+                        if Mq=="Mage":
+                            P.Create2(2)
+                            confirm(Mq)
+                            break
+                        elif Mq=="Necromancer":
+                            P.Create2(3)
+                            confirm(Mq)
+                            break
+                        elif Mq=="Spellcaster":
+                            P.Create2(4)
+                            confirm(Mq)
+                            break
+                        elif Mq=="Healer":
+                            P.Create2(5)
+                            confirm(Mq)
+                            break
+                        else:
+                            print("Error, please enter one of the options.")
+                elif Mainq=="Archer":
+                    x=True
+                    while x ==True:
+                        Aq=input("Do you want to be a Archer or Slinger (Slingshot)? ")
+                        if Aq=="Archer":
+                            P.Create2(6)
+                            confirm(Aq)
+                            break
+                        elif Aq=="Slinger":
+                            P.Create2(7)
+                            confirm(Aq)
+                            break
+                        else:
+                            print("Error, please enter one of the options.")
+                else:
+                    print("Enter a valid class.")
