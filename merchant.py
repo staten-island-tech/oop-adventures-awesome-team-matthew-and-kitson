b81ebcc514bfa7316ed868ceb319f9b6f2152146
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-01-02 21:54:25 -0500

    Delete merchant.py

--- merchant.py
+++
@@ -1,7 +0,0 @@
-class Merchant:
-    def __init__(self, products):
-        self.products=products
-    def sell(self, item):
-        self.products.remove(item)
-    def welcome(x):
-        print(f"Welcome to my shop! You can buy items from my catalogue, or sell any sellable item here!")
