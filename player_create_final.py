572a0e98e697c1d5308b3630b04d4363cc15e339
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-01-05 00:14:24 -0500

    Update player_create_final.py

--- player_create_final.py
+++ player_create_final.py
@@ -2,7 +2,6 @@ import json
 def check():
     with open("player.json", "r") as pjson:
         data=json.load(pjson)
-        while len(data)==8:
+        while len(data)==7:
             from player_create import Player
             Player.ultcreate()
-check()
