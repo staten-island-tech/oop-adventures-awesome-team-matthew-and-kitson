4602ba96d8ed35c8f38d05668e0214d583cff388
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-01-12 14:01:33 -0500

    Update player_create_final.py

--- player_create_final.py
+++ player_create_final.py
@@ -1,7 +1,15 @@
 import json
+import os
+import player_create
 def check():
     with open("player.json", "r") as pjson:
         data=json.load(pjson)
-        while len(data)==7:
-            from player_create import Player
-            Player.ultcreate()
+    if not len(data)==8:
+        data.remove(data[8])
+        new_file = "updated.json"
+        with open(new_file, "w") as pjson:
+            json_string = json.dumps(data, indent=4)
+            pjson.write(json_string)
+        os.remove("player.json")
+        os.rename(new_file, "player.json")
+    player_create.Player.ultcreate()
