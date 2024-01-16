b1a9f30a9dd27c25ef261bb1ec626e0d756e2a93
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-01-15 23:19:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from player_create import check
 check()
 from gameplay import final_wave
-print('dialogue filler')
+print('This is a generic RPG. You need to go through a series of battles known as "Waves". Each wave will have 4 battles, and there are 6 waves. If you die in any battle, you will be sent back to the first battle. When given a prompt for anything, make sure to type it completely correctly with correct capitalization or else it may be marked incorrect. If you want to challenge yourself, you can try to not use the replay command, good luck!')
 x=True
 while x==True:
     if bool(final_wave())==True:
         x=False
-print('dialogue filler')
+print('Congratulations! You beat the final boss, the Hydra, and beat the game! Hopefully that was somewhat difficult.')
