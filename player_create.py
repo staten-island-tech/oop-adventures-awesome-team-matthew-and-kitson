531c4ff7c33bebf66267dc03a5963df7d295c7c0
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-01-15 20:28:51 -0500

    Update player_create.py

--- player_create.py
+++ player_create.py
@@ -1,22 +1,21 @@
 import json
 import os
-def confirm(i):
-    print("Congratulations, you have selected & became the class:", i)
 class Player:
     with open("player.json", "r") as pjson:
         data = json.load(pjson)
-        PName=input("What is the player's name? ")
+    def confirm(i):
+        print("Congratulations, you have selected & became the class:", i)
     def Create(self, i):
-        PlayerName=Player.PName
+        PName=input("What is the player's name? ")
         Pclass=Player.data[i]['Class']
         HP=Player.data[i]['Hp']
         Weapon1=Player.data[i]['Weapon1']
         Moves=Player.data[i]['Moves']
         Weapon2=Player.data[i]['Weapon2']
         if not Weapon2 == "N/A":
-            PValues={'PName:':PlayerName,'Class:':Pclass,'BaseHP:':HP,'Weapon1:':Weapon1,'Weapon2:':Weapon2, 'Moves:': Moves, 'Gold:': [0], "HpMultiplier:":1, "DmgMultiplier:":1, "Inventory:": [Weapon1, Weapon2], "Helmet:": "N/A", "Chestplate:": "N/A", "Leggings:": "N/A", "Boots:": "N/A", "Level:": 0, "EXP:":0}
+            PValues={'PName:':PName,'Class:':Pclass,'BaseHP:':HP,'Weapon1:':Weapon1,'Weapon2:':Weapon2, 'Moves:': Moves, 'Gold:': [0], "HpMultiplier:":1, "DmgMultiplier:":1, "Inventory:": [Weapon1, Weapon2], "Helmet:": "N/A", "Chestplate:": "N/A", "Leggings:": "N/A", "Boots:": "N/A", "Level:": 0, "EXP:":0}
         else:
-            PValues={'PName:':PlayerName,'Class:':Pclass,'BaseHP:':HP,'Weapon1:':Weapon1,'Weapon2:':Weapon2, 'Moves:': Moves, 'Gold:': [0], "HpMultiplier:":1, "DmgMultiplier:":1, "Inventory:": [Weapon1], "Helmet:": "N/A", "Chestplate:": "N/A", "Leggings:": "N/A", "Boots:": "N/A", "Level:": 0, "EXP:":0}
+            PValues={'PName:':PName,'Class:':Pclass,'BaseHP:':HP,'Weapon1:':Weapon1,'Weapon2:':Weapon2, 'Moves:': Moves, 'Gold:': [0], "HpMultiplier:":1, "DmgMultiplier:":1, "Inventory:": [Weapon1], "Helmet:": "N/A", "Chestplate:": "N/A", "Leggings:": "N/A", "Boots:": "N/A", "Level:": 0, "EXP:":0}
         return(PValues)
     def Create2(self, x):
         with open("player.json", "r") as pjson:
@@ -50,12 +49,14 @@ class Player:
                     Wq=input("Do you want to be a Knight or Tank? ")
                     if Wq=="Knight":
                         P.Create2(0)
-                        confirm(Wq)
-                        break
+                        Player.confirm(Wq)
+                        e=False
+                        x=False
                     elif Wq=="Tank":
                         P.Create2(1)
-                        confirm(Wq)
-                        break
+                        Player.confirm(Wq)
+                        e=False
+                        x=False
                     else:
                         print("Error, please enter one of the options.")
             elif Mainq=="Magical":
@@ -64,20 +65,24 @@ class Player:
                     Mq=input("Do you want to be a Mage, Necromancer, Spellcaster, or Healer? ")
                     if Mq=="Mage":
                         P.Create2(2)
-                        confirm(Mq)
-                        break
+                        Player.confirm(Mq)
+                        e=False
+                        x=False
                     elif Mq=="Necromancer":
                         P.Create2(3)
-                        confirm(Mq)
-                        break
+                        Player.confirm(Mq)
+                        e=False
+                        x=False
                     elif Mq=="Spellcaster":
                         P.Create2(4)
-                        confirm(Mq)
-                        break
+                        Player.confirm(Mq)
+                        e=False
+                        x=False
                     elif Mq=="Healer":
                         P.Create2(5)
-                        confirm(Mq)
-                        break
+                        Player.confirm(Mq)
+                        e=False
+                        x=False
                     else:
                         print("Error, please enter one of the options.")
             elif Mainq=="Archer":
@@ -86,13 +91,27 @@ class Player:
                     Aq=input("Do you want to be a Archer or Slinger (Slingshot)? ")
                     if Aq=="Archer":
                         P.Create2(6)
-                        confirm(Aq)
-                        break
+                        Player.confirm(Aq)
+                        e=False
+                        x=False
                     elif Aq=="Slinger":
                         P.Create2(7)
-                        confirm(Aq)
-                        break
+                        Player.confirm(Aq)
+                        e=False
+                        x=False
                     else:
                         print("Error, please enter one of the options.")
             else:
                 print("Enter a valid class.")
+def check():
+    with open("player.json", "r") as pjson:
+        data=json.load(pjson)
+    while not len(data)==8:
+        data.remove(data[8])
+    new_file = "updated.json"
+    with open(new_file, "w") as pjson:
+        json_string = json.dumps(data, indent=4)
+        pjson.write(json_string)
+    os.remove("player.json")
+    os.rename(new_file, "player.json")
+    Player.ultcreate()
